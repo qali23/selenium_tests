a9c82be26befc3136acdfc27f0f2f90c0c249c6c
qali23 <aqureshi@softwareinstitute.com>
2024-02-23 09:17:49 +0000

    some selenium tests for Recipes webpage

--- src/test/java/com/google/SearchTest.java
+++ src/test/java/com/google/SearchTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -25,6 +26,7 @@ public class SearchTest {
         driver.manage().window().maximize();
     }
     private void sleep(long m){
+        m = 3000;//To speed up
         try{
             Thread.sleep(m);
         } catch (InterruptedException e){
@@ -51,7 +53,7 @@ public class SearchTest {
         Assert.assertNotEquals(currentUrl, url, "Searching an item and pressing enter does not change the URL?");
 
 
-        sleep(10000);
+        sleep(3000);
     }
 
     @Test(groups = { "searching" })
@@ -82,7 +84,7 @@ public class SearchTest {
         Assert.assertNotEquals(currentUrl, url, "Searching an item and clicking Google Search does not change the URL?");
 
 
-        sleep(10000);
+        sleep(3000);
     }
 
     @Test
@@ -136,6 +138,182 @@ public class SearchTest {
 
 
         sleep(10000);
+    }
+
+
+    @Test(groups = {"ultimateqa tests"})
+    public void ultimateTitleLink() {
+        //open test page
+        String url = "https://ultimateqa.com/automation";
+        driver.get(url);
+
+        WebElement companyLogo = driver.findElement(By.xpath("/html//div[@id='main-menu']//a[@href='https://ultimateqa.com/']/img"));
+        String initialURL = driver.getCurrentUrl();
+        companyLogo.click();
+        sleep(3000);
+        String afterURL = driver.getCurrentUrl();
+
+        Assert.assertEquals(afterURL, "https://ultimateqa.com/");
+        Assert.assertNotEquals(initialURL, afterURL);
+
+        sleep(10000);
+    }
+
+
+
+    @Test(groups = { "Recipes Webpage" })
+    public void recipesHomepage() {
+        //open test page
+        String url = "http://localhost:3000/";
+        driver.get(url);
+        Actions actions = new Actions(driver);
+
+        //Hover mouse over image
+        WebElement homepageLink = driver.findElement(By.id("recipesHomepage"));
+        homepageLink.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, "http://localhost:3000/recipe_homepage", "Link not working properly");
+
+        sleep(3000);
+    }
+
+    @Test(groups = { "Recipes Webpage" })
+    public void allRecipesPage() {
+        //open test page
+        String url = "http://localhost:3000/";
+        driver.get(url);
+        Actions actions = new Actions(driver);
+
+        //Hover mouse over image
+        WebElement allRecipesLink = driver.findElement(By.id("allRecipes"));
+        allRecipesLink.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, "http://localhost:3000/", "Link not working properly");
+
+        sleep(5000);
+    }
+
+    @Test(groups = { "Recipes Webpage" })
+    public void ingredientsPage() {
+        //open test page
+        String url = "http://localhost:3000/";
+        driver.get(url);
+        Actions actions = new Actions(driver);
+
+        //Hover mouse over image
+        WebElement ingredientsLink = driver.findElement(By.id("ingredients"));
+        ingredientsLink.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, "http://localhost:3000/ingredients", "Link not working properly");
+
+        sleep(5000);
+    }
+
+    @Test(groups = { "Recipes Webpage" })
+    public void chefsPage() {
+        //open test page
+        String url = "http://localhost:3000/";
+        driver.get(url);
+        Actions actions = new Actions(driver);
+
+        //Hover mouse over image
+        WebElement chefsLink = driver.findElement(By.id("chefs"));
+        chefsLink.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, "http://localhost:3000/chefs", "Link not working properly");
+
+        sleep(5000);
+    }
+
+    @Test(groups = { "Recipes Webpage" })
+    public void imageHover() {
+        //open test page
+        String url = "http://localhost:3000/";
+        driver.get(url);
+        Actions actions = new Actions(driver);
+
+        //Hover mouse over image
+        WebElement yzmaImage = driver.findElement(By.id("yzma"));
+        String initialSource = yzmaImage.getAttribute("src");
+        actions.moveToElement(yzmaImage).perform();
+        String hoverSource = yzmaImage.getAttribute("src");
+
+        Assert.assertNotEquals(initialSource, hoverSource, "Hovering over Yzma image does not change it.");
+
+
+        sleep(3000);
+    }
+
+    @Test(groups = { "Recipes Webpage" })
+    public void newDishSection() {
+        //open test page
+        String url = "http://localhost:3000/";
+        driver.get(url);
+        Actions actions = new Actions(driver);
+
+        //Check if new recipe is added successfully
+        WebElement dishNameInput = driver.findElement(By.id("fname"));
+        dishNameInput.sendKeys("Rick-ar-Dough Shires Bread Recipe");
+        WebElement recipeInput = driver.findElement(By.id("lname"));
+        recipeInput.sendKeys("450g flour + 350ml water + 1 tsp yeast + 1 tsp salt. Mix and leave for 8ish hours. Bake for 50ish mins.");
+        WebElement submitButton = driver.findElement(By.id("submit_button"));
+        submitButton.click();
+
+
+        Assert.assertNotNull(driver.findElement(By.id("Rick-ar-Dough Shires Bread Recipe")));
+
+        sleep(3000);
+    }
+
+    @Test( dependsOnMethods = { "newDishSection" }, groups = { "Recipes Webpage" })
+    public void newDishLink() {
+        //open test page
+        String url = "http://localhost:3000/";
+        driver.get(url);
+        Actions actions = new Actions(driver);
+
+        //Check if new recipe link is added successfully
+        WebElement dishNameInput = driver.findElement(By.id("fname"));
+        dishNameInput.sendKeys("Rick-ar-Dough Shires Bread Recipe");
+        WebElement recipeInput = driver.findElement(By.id("lname"));
+        recipeInput.sendKeys("450g flour + 350ml water + 1 tsp yeast + 1 tsp salt. Mix and leave for 8ish hours. Bake for 50ish mins.");
+        WebElement submitButton = driver.findElement(By.id("submit_button"));
+        submitButton.click();
+        WebElement newLink = driver.findElement(By.id("Rick-ar-Dough Shires Bread Recipe_link"));
+        newLink.click();
+
+        Assert.assertNotNull(driver.findElement(By.id("Rick-ar-Dough Shires Bread Recipe")));
+
+        sleep(3000);
+    }
+    @Test(dependsOnMethods = { "newDishSection", "newDishLink" }, groups = { "Recipes Webpage" })
+    public void multipleNewDish() {
+        //open test page
+        String url = "http://localhost:3000/";
+        driver.get(url);
+        Actions actions = new Actions(driver);
+
+        WebElement dishNameInput = driver.findElement(By.id("fname"));
+        WebElement recipeInput = driver.findElement(By.id("lname"));
+        WebElement submitButton = driver.findElement(By.id("submit_button"));
+
+        //Check if many new recipes are added successfully
+        for(int i =0; i<5; i++){
+            dishNameInput.sendKeys("Rick-ar-Dough Shires Bread Recipe"+ i);
+            recipeInput.sendKeys("450g flour + 350ml water + 1 tsp yeast + 1 tsp salt. Mix and leave for 8ish hours. Bake for 50ish mins." + i);
+            submitButton.click();
+            WebElement newLink = driver.findElement(By.id("Rick-ar-Dough Shires Bread Recipe" + i + "_link"));
+            newLink.click();
+            Assert.assertNotNull(driver.findElement(By.id("Rick-ar-Dough Shires Bread Recipe"+i)));
+            sleep(3000);
+            dishNameInput.clear();
+            recipeInput.clear();
+        }
+        sleep(3000);
     }
 
     @AfterMethod(alwaysRun = true)
